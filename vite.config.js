77a288416fc40c0db23a88cf7d5f5e2cbe795aa3
dhimanrobin310-droid <dhimanrobin310@gmail.com>
2026-08-22 04:45:18 -0700

    upd

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://ecom-1hqx.onrender.com',
+        target: 'http://localhost:9000',
         changeOrigin: true,
         secure: false,
       },
